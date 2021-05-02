ac4d2ae066091f5253da89a34eb178b91d574a70
Bhaskar <b2gogoi@hotmail.com>
2021-05-02 13:06:47 +0530

    Theme palette with black background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import {
   AppBar,
   IconButton,
@@ -6,13 +8,65 @@ import {
   Toolbar
 } from '@material-ui/core';
 
+// Create your Own theme:
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#000',
+      contrastText: '#FFED94'
+    },
+    secondary: {
+      main: '#E7E7E7',
+      contrastText: '#1C365A'
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Poppins',
+      'Roboto',
+      'Arial',
+      'sans-serif'
+    ].join(','),
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          backgroundColor: '#000',
+        },
+      },
+    },
+    MuiDialog: {
+      paper: {
+        borderRadius: 22,
+      },
+    },
+    MuiOutlinedInput: {
+      root: {
+        borderRadius: 12,
+      },
+      adornedStart: {
+        borderRadius: 22,
+      }
+    }
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <AppBar position="fixed">
-        UMG
-      </AppBar>UMG
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <AppBar position="fixed">
+          UMG
+        </AppBar>UMG
+      </div>
+    </MuiThemeProvider>
+    
   );
 }
 
